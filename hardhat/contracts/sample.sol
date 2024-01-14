0dc7f1fb400e09cc5292d413312fecd76ad7a5ae
lvdthieu <luuvanducthieu291@gmail.com>
2024-01-14 10:16:56 +0700

    Update

--- hardhat/contracts/sample.sol
+++ hardhat/contracts/sample.sol
@@ -1,69 +1,53 @@
-contract ThisExternalAssembly {
-    uint public numcalls;
-    uint public numcallsinternal;
-    uint public numfails;
-    uint public numsuccesses;
-    
-    address owner;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-    event logCall(uint indexed _numcalls, uint indexed _numcallsinternal);
-    
-    modifier onlyOwner { if (msg.sender != owner) throw; _ }
-    modifier onlyThis { if (msg.sender != address(this)) throw; _ }
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-    // constructor
-    function ThisExternalAssembly() {
-        owner = msg.sender;
+contract FrenshipToken is ERC20 {
+    address private admin;
+    mapping(address=>bool) private minters;
+    uint256 maxSupply = 300000000;
+
+    constructor () ERC20 ("FrenshipToken", "Frenship") {
+       admin = msg.sender;
     }
 
-    function failSend() external onlyThis returns (bool) {
-        // storage change + nested external call
-        numcallsinternal++;
-        owner.send(42);
-
-        // placeholder for state checks
-        if (true) throw;
-
-        // never happens in this case
-        return true;
-    }
-    
-    function doCall(uint _gas) onlyOwner {
-        numcalls++;
-
-        address addr = address(this);
-        bytes4 sig = bytes4(sha3("failSend()"));
-
-        bool ret;
-
-        // work around `solc` safeguards for throws in external calls
-        // https://ethereum.stackexchange.com/questions/6354/
-        assembly {
-            let x := mload(0x40) // read "empty memory" pointer
-            mstore(x,sig)
-
-            ret := call(
-                _gas, // gas amount
-                addr, // recipient account
-                0,    // value (no need to pass)
-                x,    // input start location
-                0x4,  // input size - just the sig
-                x,    // output start location
-                0x1)  // output size (bool - 1 byte)
-
-            //ret := mload(x) // no return value ever written :/
-            mstore(0x40,add(x,0x4)) // just in case, roll the tape
-        }
-
-        if (ret) { numsuccesses++; }
-        else { numfails++; }
-
-        // mostly helps with function identification if disassembled
-        logCall(numcalls, numcallsinternal);
+    function setMaxSupply(uint256 newMaxSupply) public adminOnly{
+        maxSupply = newMaxSupply;
     }
 
-    // will clean-up :)
-    function selfDestruct() onlyOwner { selfdestruct(owner); }
+    function setMinter(address _minter) public adminOnly {
+        minters[_minter] = true;
+    }
+
+    function devMint(address to, uint256 amount) public adminOnly {
+        uint256 totalSupply = totalSupply();
+
+        require(totalSupply + amount <= maxSupply, "reached max supply");
+
+        _mint(to, amount);
+    }
+
+    function mint(address to, uint256 amount) public {
+        require(minters[msg.sender], "minter only");
+        
+        uint256 totalSupply = totalSupply();
+
+        require(totalSupply + amount <= maxSupply, "reached max supply");
+
+        _mint(to, amount);
+    }
+
+    function burn(uint amount) external {
+        _burn(msg.sender, amount);
+    }
     
-    function() { throw; }
+    function decimals() public view virtual override returns (uint8) {
+        return 0;
+    }
+
+     modifier adminOnly() {
+        require(admin == msg.sender, "not admin");
+        _;
+    }
 }
