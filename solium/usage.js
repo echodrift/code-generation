e53fe6bb3060c7c2e902c5753510a2e68bf0ea03
lvdthieu <luuvanducthieu291@gmail.com>
2024-01-22 22:57:18 +0700

    Test solium static analyzer

---
+++ solium/usage.js
@@ -0,0 +1,18 @@
+import solium from "solium"
+import fs from "fs"
+
+const source = fs.readFileSync("./contracts/check.sol")
+
+const erros = solium.lint(source, {
+    "extends": "solium:recommended",
+    "plugins": ["security"],
+    "rules": {
+            "quotes": ["error", "double"],
+            "double-quotes": [2],   // returns a rule deprecation warning
+            "pragma-on-top": 1
+    },
+
+    "options": { "returnInternalIssues": true }
+})
+
+erros.foreach(console.log)
