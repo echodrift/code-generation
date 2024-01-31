99faf690e63353b9bf0604f55f8aa2231ad4cfe8
lvdthieu <luuvanducthieu291@gmail.com>
2024-01-31 22:46:54 +0700

    Make a pipeline to check compilable rate

--- compilation/compile.py
+++ compilation/compile.py
@@ -8,39 +8,34 @@ base = os.path.dirname(os.path.abspath(__file__))
 compiler = os.path.join(base, "compilers")
 
 
-def compile(input: str, hardhat: str, output: str, error_path: str):
+def compile(input: str, hardhat: str, output: str):
     """Compile a solidity file
 
     Args:
         input (str): Parquet file store solidity files path
         hardhat (str): Hardhat compiler path
-        output (str): Compile result path
-        error_path (str): Compile error file path
+        output (str): Compile info file path
     """
-    compilable = []
-    uncompilable = []
+    compile_info = []
     test_compile = pd.read_parquet(input, engine="fastparquet")
     for i in tqdm(test_compile.index):
         source = test_compile.loc[i, "source_code"]
         with open(os.path.join(compiler, hardhat, "contracts", "sample.sol"), "w") as f:
             f.write(source)
+
         cmd = f"""
         cd {os.path.join(compiler, hardhat)}
         npx hardhat compile --force
         """
         data = run(cmd, shell=True, capture_output=True, text=True)
         if "Compiled 1 Solidity file successfully" in data.stdout:
-            compilable.append(i)
-            uncompilable.append("<COMPILED_SUCCESSFULLY>")
+            compile_info.append("<COMPILED_SUCCESSFULLY>")
         else:
-            uncompilable.append(data.stderr)
-
-    compilable = test_compile[test_compile.index.isin(compilable)]
-    compilable.to_parquet(output, engine="fastparquet")
+            compile_info.append(data.stderr)
 
     error = test_compile
-    error["compile_info"] = uncompilable
-    error.to_parquet(error_path, engine="fastparquet")
+    error["compile_info"] = compile_info
+    error.to_parquet(output, engine="fastparquet")
 
 
 if __name__ == "__main__":
@@ -48,11 +43,9 @@ if __name__ == "__main__":
     parser.add_argument("-i", "--input", dest="input")
     parser.add_argument("-hh", "--hardhat", dest="hardhat")
     parser.add_argument("-o", "--output", dest="output")
-    parser.add_argument("-e", "--error", dest="error")
     args = parser.parse_args()
     compile(
         input=args.input,
         hardhat=args.hardhat,
         output=args.output,
-        error_path=args.error,
     )
--- compilation/compile.sh
+++ compilation/compile.sh
@@ -12,36 +12,36 @@ COMPILE_INFO=$4
 
 # Compile 
 
-i=1
-while [ $i -le $CONCURRENCY ]; do
-    if [ ! -d "$COMPILER/hardhat$i" ]; then
-        cp -r $COMPILER/hardhat $COMPILER/hardhat$i
-        echo Copied to $COMPILER/hardhat$i
-    else
-        if [ -d "$COMPILER/hardhat$i/artifacts" ]; then
-            rm -r $COMPILER/hardhat$i/artifacts
-        fi
-        if [ -d "$COMPILER/hardhat$i/cache" ]; then
-            rm -r $COMPILER/hardhat$i/cache
-        fi
-    fi
-    i=$((i + 1))
-done
-echo Create folder done
-python 
-python $DIR/../tools/sharding.py --input $INPUT --concurrency $CONCURRENCY --output $DIR/data
-echo Sharding done
+# i=1
+# while [ $i -le $CONCURRENCY ]; do
+#     if [ ! -d "$COMPILER/hardhat$i" ]; then
+#         cp -r $COMPILER/hardhat $COMPILER/hardhat$i
+#         echo Copied to $COMPILER/hardhat$i
+#     else
+#         if [ -d "$COMPILER/hardhat$i/artifacts" ]; then
+#             rm -r $COMPILER/hardhat$i/artifacts
+#         fi
+#         if [ -d "$COMPILER/hardhat$i/cache" ]; then
+#             rm -r $COMPILER/hardhat$i/cache
+#         fi
+#     fi
+#     i=$((i + 1))
+# done
+# echo Create folder done
+# python $DIR/../tools/funcs.py --func sharding --input $INPUT --concurrency $CONCURRENCY --output $DIR/data
+# echo Sharding done
 
-i=1
-while [ $i -le $CONCURRENCY ]; do
-    screen -dmS hardhat$i bash -c "python compile.py -i $DIR/data/batch$i.parquet -hh hardhat$i -o $DIR/out/result$i.parquet -e $DIR/error/result$i.parquet"
-    i=$(( i + 1))
-done
+# i=1
+
+# while [ $i -le $CONCURRENCY ]; do
+#     screen -dmS hardhat$i bash -c "python compile.py -i $DIR/data/batch$i.parquet -hh hardhat$i -o $DIR/out/result$i.parquet -e $DIR/error/result$i.parquet"
+#     i=$(( i + 1))
+# done
 
 ############################################################################################################
 
 # Merging
 
-python $DIR/../tools/merging.py --input $DIR/out --concurrency $CONCURRENCY --output $OUTPUT 
-python $DIR/../tools/merging.py --input $DIR/error --concurrency $CONCURRENCY --output $COMPILE_INFO
+python $DIR/../tools/funcs.py --func merging --input $DIR/out --concurrency $CONCURRENCY --output $OUTPUT 
+python $DIR/../tools/funcs.py --func merging --input $DIR/error --concurrency $CONCURRENCY --output $COMPILE_INFO
 echo Merging done
