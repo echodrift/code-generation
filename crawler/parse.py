4ae632dd675308be9711d997402f6eea013e04df
lvdthieu <luuvanducthieu291@gmail.com>
2024-03-25 23:49:57 +0700

    Add crawler/parse.py to implement function that mask java class a function body

--- crawler/parse.py
+++ crawler/parse.py
@@ -2,109 +2,118 @@ from antlr4 import *
 from java.java8.JavaLexer import JavaLexer
 from java.java8.JavaParser import JavaParser
 from java.java8.JavaParserListener import JavaParserListener
+import pandas as pd
+import random
+from typing import Tuple, Optional
+from collections import namedtuple
+from tqdm import tqdm
+
+random.seed(0)
+ASample = namedtuple("ASample", "class_name func_name masked_class func_body")
+Location = namedtuple("Location", "start_line start_col end_line end_col")
+
+class Extractor(JavaParserListener):
+    def __init__(self, token_stream):
+        super().__init__()
+        self.functions = []
+        self.token_stream = token_stream
+    
+    def enterClassDeclaration(self, ctx):
+        self.class_name = ctx.identifier().getText()
+        self.class_loc = Location(ctx.start.line, ctx.start.column, ctx.stop.line, 
+                                  ctx.stop.column + len(ctx.stop.text))
+
+    def enterMethodDeclaration(self, ctx):
+        self.func_name = ctx.identifier().getText()
+        body = ctx.methodBody().block()
+        if not body:
+            return
+        self.func_body_loc = Location(body.start.line, body.start.column, 
+                                      body.stop.line, body.stop.column + len(body.stop.text))
+        try:
+            self.functions.append({"class_name": self.class_name, 
+                                "class_loc": self.class_loc,
+                                "func_name": self.func_name,
+                                "func_body_loc": self.func_body_loc})
+        except:
+            pass
+                            
+        
+    def get_function(self):
+        return self.functions
+        
+        
+def get_location(java_code: str, loc: Location) -> Tuple[int, int]:
+    lines = java_code.split("\n")
+    start_idx = 0
+    for i in range(loc.start_line - 1):
+        start_idx += len(lines[i])
+    start_idx = start_idx + loc.start_col + loc.start_line - 1
+
+    end_idx = 0
+    for i in range(loc.end_line - 1):
+        end_idx += len(lines[i])
+    end_idx = end_idx + loc.end_col + loc.end_line - 1
+    return start_idx, end_idx
 
 
+def mask_function(java_code: str) -> Optional[ASample]:
+    input_stream = InputStream(java_code)
+    lexer = JavaLexer(input_stream)
+    token_stream = CommonTokenStream(lexer)
+    parser = JavaParser(token_stream)
+    tree = parser.compilationUnit()
+    # Create listener
+    listener = Extractor(token_stream)
+    # Walk the parse tree
+    walker = ParseTreeWalker()
+    walker.walk(listener, tree)
+    functions = listener.get_function()
 
-# class LocationExtractor(JavaParserListener):
-#     def __init__(self, token_stream):
-#         super().__init__()
-#         self.token_stream = token_stream
+    # If file has no function, return None
+    if not functions:
+        return None
+    
+    # Randomly select a function 
+    random_function = random.choice(functions)
+    
+    # Extract function body 
+    class_start_idx, class_end_idx = get_location(java_code, random_function["class_loc"])
+    func_body_start_idx, func_body_end_idx = get_location(java_code, random_function["func_body_loc"])
+    masked_class = java_code[class_start_idx : func_body_start_idx + 1] + "<FILL_FUNCTION_BODY>" \
+                    + java_code[func_body_end_idx - 1 : class_end_idx]
+    func_body = java_code[func_body_start_idx + 1 : func_body_end_idx - 1]
 
-#     def print_location(self, ctx):
-#         start_token = ctx.start
-#         start_line = start_token.line
-#         start_column = start_token.column
-#         end_token = ctx.stop
-#         end_line = end_token.line
-#         end_column = end_token.column + len(end_token.text)
+    return ASample(class_name=random_function["class_name"], 
+                   func_name=random_function["func_name"], 
+                   masked_class=masked_class, 
+                   func_body=func_body)
 
-#         print(f"Name: {ctx.identifier().getText()}")
-#         print(f"Start location: Line {start_line}, Column {start_column}")
-#         print(f"End location: Line {end_line}, Column {end_column}")
 
-#     def enterMethodDeclaration(self, ctx):
-#         self.print_location(ctx)
+def make_dataset(java_file_urls_storage_url: str) -> pd.DataFrame:
+    rows = []
+    with open(java_file_urls_storage_url, "r") as f:
+        java_file_urls = map(lambda url: url.strip(), f.readlines())
+    
+    for java_file_url in tqdm(java_file_urls):
+        project_name = java_file_url[37:].split('/')[0]
+        relative_path = '/'.join(java_file_url[37:].split('/')[1:])
+        with open(java_file_url, "r") as f:
+            java_code = f.read()
+            sample = mask_function(java_code)
+            if sample:
+                rows.append({
+                    "proj_name": project_name,
+                    "relative_path": relative_path,
+                    "class_name": sample.class_name,
+                    "func_name": sample.func_name,
+                    "masked_class": sample.masked_class,
+                    "func_body": sample.func_body
+                })
+    return pd.DataFrame(rows)
     
 
-# def main():
-#     for proj in TEST_PROJ:
-#         pass
-#     java_code = """
-# /*
-# * Copyright (c) 2013-2017 Chris Newland.
-# * Licensed under https://github.com/AdoptOpenJDK/jitwatch/blob/master/LICENSE-BSD
-# * Instructions: https://github.com/AdoptOpenJDK/jitwatch/wiki
-# */
-# package org.adoptopenjdk.jitwatch.core;
-
-# import static org.adoptopenjdk.jitwatch.core.JITWatchConstants.S_CLOSE_PARENTHESES;
-# import static org.adoptopenjdk.jitwatch.core.JITWatchConstants.S_NEWLINE;
-# import static org.adoptopenjdk.jitwatch.core.JITWatchConstants.S_OPEN_PARENTHESES;
-# import static org.adoptopenjdk.jitwatch.core.JITWatchConstants.S_SPACE;
-
-# import java.util.LinkedHashMap;
-# import java.util.Map;
-
-# public class ErrorLog
-# {
-#     private Map<String, Integer> errorCountMap = new LinkedHashMap<>();
-
-#     public void clear()
-#     {
-#         errorCountMap.clear();
-#     }
-
-#     public void addEntry(String entry)
-#     {
-#         if (errorCountMap.containsKey(entry))
-#         {
-#             errorCountMap.put(entry, errorCountMap.get(entry) + 1);
-#         }
-#         else
-#         {
-#             errorCountMap.put(entry, 1);
-#         }
-#     }
-
-#     @Override
-#     public String toString()
-#     {
-#         StringBuilder builder = new StringBuilder();
-
-#         for (Map.Entry<String, Integer> entry : errorCountMap.entrySet())
-#         {
-#             String msg = entry.getKey();
-#             int count = entry.getValue();
-
-#             if (count == 1)
-#             {
-#                 builder.append(msg).append(S_NEWLINE);
-#             }
-#             else
-#             {
-#                 builder.append(msg).append(S_SPACE).append(S_OPEN_PARENTHESES).append(count).append(S_SPACE).append("times").append(S_CLOSE_PARENTHESES).append(S_NEWLINE);
-#             }
-#         }
-
-#         return builder.toString();
-
-#     }
-# }
-#     """
-#     input_stream = InputStream(java_code)
-#     lexer = JavaLexer(input_stream)
-#     token_stream = CommonTokenStream(lexer)
-#     parser = JavaParser(token_stream)
-#     tree = parser.compilationUnit()
-#     # Create listener
-#     listener = LocationExtractor(token_stream)
-
-#     # Walk the parse tree
-#     walker = ParseTreeWalker()
-#     walker.walk(listener, tree)
-def preprocess(java_file_urls_storage_url: str):
-    with open(java_file_urls_storage_url, "r") as f:
-        java_file_urls = list(map(lambda java_file_url: java_file_url[37:],f.readlines()))
 
 if __name__ == "__main__":
-    preprocess("/home/hieuvd/lvdthieu/CodeGen/crawler/java_file.txt")
+    df = make_dataset("/home/hieuvd/lvdthieu/CodeGen/crawler/java_file.txt")
+    df.to_parquet("dataset.parquet", "fastparquet")
