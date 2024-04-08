a0612c54217c67ccd953244c21cdde533ccc601c
lvdthieu <luuvanducthieu291@gmail.com>
2024-04-08 10:40:40 +0700

    Add extract inherit element for java code function

--- java_data/parse.py
+++ java_data/parse.py
@@ -1,8 +1,9 @@
 import argparse
 import codecs
+import json
 import random
 import re
-from collections import defaultdict, namedtuple
+from collections import defaultdict
 from typing import List, NamedTuple, Optional, Tuple
 
 import pandas as pd
@@ -11,6 +12,7 @@ from java.java8.JavaLexer import JavaLexer
 from java.java8.JavaParser import JavaParser
 from java.java8.JavaParserListener import JavaParserListener
 from tqdm import tqdm
+
 
 ASample = NamedTuple("ASample", [("class_name", str), ("func_name", str), ("masked_class", str), ("func_body", str)])
 Location = NamedTuple("Location", [("start_line", int), ("start_col", int), ("end_line", int), ("end_col", int)])
@@ -46,40 +48,28 @@ class ExtractFunc(JavaParserListener):
         return self.functions
 
 
-class ExtractParentComponent(JavaParserListener):
-    def __init__(self, class_name):
-        super().__init__()
-        self.class_name = class_name
-        self.parent_class = None
-
-    def enterClassDeclaration(self, ctx):
-        if self.class_name == ctx.identifier().getText():
-            if ctx.typeType():
-                self.parent_class = ctx.typeType().getText()
-
-    def get_parent_class(self):
-        return self.parent_class
-
-   
 class ExtractSignatureAndVar(JavaParserListener):
-    def __init__(self):
+    def __init__(self, java_code: str):
         super().__init__()
         self.class_comp = defaultdict(list)
-    
+        self.java_code = java_code
+
     def enterClassDeclaration(self, ctx):
         self.class_name = ctx.identifier().getText()
 
     def enterMethodDeclaration(self, ctx):
         self.func_name = ctx.identifier().getText()
         body = ctx.methodBody().block()
-        func_body_start_idx, _ = get_location(Location(body.start.line, body.start.column, 
+        func_body_start_idx, _ = get_location(self.java_code, Location(body.start.line, body.start.column, 
                                                        body.stop.line, body.stop.column + len(body.stop.text)))
-        func_start_idx, _ = get_location(Location(ctx.start.line, ctx.start.column,
+        func_start_idx, _ = get_location(self.java_code, Location(ctx.start.line, ctx.start.column,
                                                   ctx.stop.line, ctx.stop.column + len(ctx.stop.text)))
-        self.class_comp[self.class_name].append((func_start_idx, func_body_start_idx))
+        self.class_comp[self.class_name].append(self.java_code[func_start_idx : func_body_start_idx])
     
-    def enterVariableDeclaratorId(self, ctx):
-        return super().enterVariableDeclaratorId(ctx)
+    def enterFieldDeclaration(self, ctx):
+        variable_name = ctx.variableDeclarators().getText()
+        variable_type = ctx.typeType().getText()
+        self.class_comp[self.class_name].append("%s %s;" % (variable_type, variable_name))
 
     def get_class_comp(self):
         return self.class_comp
@@ -212,8 +202,8 @@ def post_processing(dataset: pd.DataFrame) -> pd.DataFrame:
 
 
 def fill_generated_code_to_file(generated_func_dataset: pd.DataFrame, 
-                                 generated_func_column: str, 
-                                 project_storage_url: str) -> pd.DataFrame:
+                                generated_func_column: str, 
+                                project_storage_url: str) -> pd.DataFrame:
     tqdm.pandas()
     def fill_file(row):
         absolute_file_path = "{}/{}/{}".format(project_storage_url, row["proj_name"], row["relative_path"])
@@ -234,52 +224,60 @@ def fill_generated_code_to_file(generated_func_dataset: pd.DataFrame,
     return generated_func_dataset
 
 
-def extract_parent_component(java_code: str, proj_name: str, class_name: str):
-    # parsed_file = file_url.replace(".txt", ".json")
-    # with open(parsed_file, "r") as f:
-    #     ast = json.load(f)
-    # super_class_name = None
-    # for cls in ast["types"]:
-    #     if not cls["interface"] and cls["name"]["identifier"] == class_name:
-    #         if cls["superclassType"]:
-    #             super_class_name = cls["superclassType"]["name"]["identifier"]
-    #             break
-    # else:
-    #     return None
-    # try:
-        input_stream = InputStream(java_code)
-        lexer = JavaLexer(input_stream)
-        token_stream = CommonTokenStream(lexer)
-        parser = JavaParser(token_stream)
-        tree = parser.compilationUnit()
-        # Create listener
-        listener = ExtractParentComponent(class_name)
-        # Walk the parse tree
-        walker = ParseTreeWalker()
-        walker.walk(listener, tree)
-        parent = listener.get_parent_class()
+def search_parent_class(df: pd.DataFrame, storage_url: str) -> pd.DataFrame:
+    def get_parent_class(row, storage_url: str):
+        parsed_project_path = "{}/parsed_{}.json".format(storage_url, row["proj_name"])
+        with open(parsed_project_path, "r") as f:
+            class_info = json.load(f)
+        for cls in class_info:
+            if (cls["classInfos"]["filePath"] == "{}/{}".format(row["proj_name"], row["relative_path"]) and
+                cls["classInfos"]["className"] == row["class_name"]):
+                if cls["classInfos"]["extendedClassQualifiedName"] not in ["", "java.lang.Object"]:
+                    parent_class = cls["classInfos"]["extendedClassQualifiedName"]
+                else:
+                    parent_class = ""
+                break
+        else:
+            parent_class = ""
         
-        if not parent:
+        if parent_class == "":
             return ""
         else:
-            pass
+            for cls in class_info:
+                if (cls["classInfos"]["QualifiedName"] == parent_class):
+                    return cls["classInfos"]["sourceCode"]
+            return ""
+    tqdm.pandas()
+    df["parent_class_code"] = df.progress_apply(lambda row: get_parent_class(row, storage_url), axis=1)
+    return df
 
 
-def get_signature_component(java_code: str):
+def extract_signature_and_var(java_code: str):
+    if not java_code:
+        return ""
     input_stream = InputStream(java_code)
     lexer = JavaLexer(input_stream)
     token_stream = CommonTokenStream(lexer)
     parser = JavaParser(token_stream)
     tree = parser.compilationUnit()
     # Create listener
-    listener = ExtractSignatureAndVar()
+    listener = ExtractSignatureAndVar(java_code)
     # Walk the parse tree
     walker = ParseTreeWalker()
     walker.walk(listener, tree)
     class_comps = listener.get_class_comp()
-    print(class_comps)
-    
-        
+    return json.dumps(class_comps)
+
+
+def get_parent_signature_and_var(df: pd.DataFrame, storage_url: str) -> pd.DataFrame:
+    new_df = search_parent_class(df, storage_url)
+    new_df["inherit_elements"] = new_df["parent_class_code"].apply(extract_signature_and_var)
+    return new_df
+
+
+def store_dataset(df: pd.DataFrame, storage_url: str):
+    df.to_parquet(storage_url, "fastparquet")
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", dest="input")
@@ -291,178 +289,16 @@ if __name__ == "__main__":
     args = parser.parse_args()
     
     match args.func:
-        case "md":
+        case "md":  # Make Dataset
             df = post_processing(make_dataset(java_file_urls_storage_url=args.input, checkpoint=args.checkpoint))
-            df.to_parquet(args.output, "fastparquet")
-        case "ff":
+            store_dataset(df, args.output)
+        case "ff":  # Fill Generated Code
             df = pd.read_parquet(args.input, "fastparquet")
             df = fill_generated_code_to_file(df, args.col, args.dir)
-            df.to_parquet(args.output, "fastparquet")
-        case "epc":
-            java_code = \
-"""
-/*******************************************************************************
- *     ___                  _   ____  ____
- *    / _ \ _   _  ___  ___| |_|  _ \| __ )
- *   | | | | | | |/ _ \/ __| __| | | |  _ \
- *   | |_| | |_| |  __/\__ \ |_| |_| | |_) |
- *    \__\_\\__,_|\___||___/\__|____/|____/
- *
- *  Copyright (c) 2014-2019 Appsicle
- *  Copyright (c) 2019-2023 QuestDB
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
- ******************************************************************************/
-
-package io.questdb.cairo.frm.file;
-
-import io.questdb.cairo.CairoConfiguration;
-import io.questdb.cairo.ColumnType;
-import io.questdb.cairo.frm.FrameColumn;
-import io.questdb.cairo.frm.FrameColumnPool;
-import io.questdb.cairo.frm.FrameColumnTypePool;
-import io.questdb.std.FilesFacade;
-import io.questdb.std.ObjList;
-import io.questdb.std.str.Path;
-
-import java.io.Closeable;
-
-public class ContiguousFileColumnPool implements FrameColumnPool, Closeable {
-    private final ColumnTypePool columnTypePool = new ColumnTypePool();
-    private final CairoConfiguration configuration;
-    private final FilesFacade ff;
-    private final ListPool<ContiguousFileFixFrameColumn> fixColumnPool = new ListPool<>();
-    private final ListPool<ContiguousFileFixFrameColumn> indexedColumnPool = new ListPool<>();
-    private final ListPool<ContiguousFileVarFrameColumn> varColumnPool = new ListPool<>();
-    private boolean canWrite;
-    private boolean isClosed;
-
-    public ContiguousFileColumnPool(CairoConfiguration configuration) {
-        this.ff = configuration.getFilesFacade();
-        this.configuration = configuration;
-    }
-
-    @Override
-    public void close() {
-        this.isClosed = true;
-    }
-
-    @Override
-    public FrameColumnTypePool getPoolRO(int columnType) {
-        this.canWrite = false;
-        return columnTypePool;
-    }
-
-    @Override
-    public FrameColumnTypePool getPoolRW(int columnType) {
-        this.canWrite = true;
-        return columnTypePool;
-    }
-
-    private class ColumnTypePool implements FrameColumnTypePool {
-
-        @Override
-        public FrameColumn create(
-                Path partitionPath,
-                CharSequence columnName,
-                long columnTxn,
-                int columnType,
-                int indexBlockCapacity,
-                long columnTop,
-                int columnIndex,
-                boolean isEmpty
-        ) {
-            boolean isIndexed = indexBlockCapacity > 0;
-
-            if (ColumnType.isVarSize(columnType)) {
-                ContiguousFileVarFrameColumn column = getVarColumn();
-                if (canWrite) {
-                    column.ofRW(partitionPath, columnName, columnTxn, columnType, columnTop, columnIndex);
-                } else {
-                    column.ofRO(partitionPath, columnName, columnTxn, columnType, columnTop, columnIndex, isEmpty);
-                }
-                return column;
-            }
-
-            if (columnType == ColumnType.SYMBOL) {
-                if (canWrite && isIndexed) {
-                    ContiguousFileIndexedFrameColumn indexedColumn = getIndexedColumn();
-                    indexedColumn.ofRW(partitionPath, columnName, columnTxn, columnType, indexBlockCapacity, columnTop, columnIndex, isEmpty);
-                    return indexedColumn;
-                }
-            }
-
-            ContiguousFileFixFrameColumn column = getFixColumn();
-            if (canWrite) {
-                column.ofRW(partitionPath, columnName, columnTxn, columnType, columnTop, columnIndex);
-            } else {
-                column.ofRO(partitionPath, columnName, columnTxn, columnType, columnTop, columnIndex, isEmpty);
-            }
-            return column;
-        }
-
-        private ContiguousFileFixFrameColumn getFixColumn() {
-            if (fixColumnPool.size() > 0) {
-                return fixColumnPool.pop();
-            }
-            ContiguousFileFixFrameColumn col = new ContiguousFileFixFrameColumn(configuration);
-            col.setPool(fixColumnPool);
-            return col;
-        }
-
-        private ContiguousFileIndexedFrameColumn getIndexedColumn() {
-            if (indexedColumnPool.size() > 0) {
-                return (ContiguousFileIndexedFrameColumn) indexedColumnPool.pop();
-            }
-            ContiguousFileIndexedFrameColumn col = new ContiguousFileIndexedFrameColumn(configuration);
-            col.setPool(indexedColumnPool);
-            return col;
-        }
-
-        private ContiguousFileVarFrameColumn getVarColumn() {
-            if (varColumnPool.size() > 0) {
-                return varColumnPool.pop();
-            }
-            ContiguousFileVarFrameColumn col = new ContiguousFileVarFrameColumn(configuration);
-            col.setPool(varColumnPool);
-            return col;
-        }
-    }
-
-    private class ListPool<T> implements RecycleBin<T> {
-        private final ObjList<T> pool = new ObjList<>();
-
-        @Override
-        public boolean isClosed() {
-            return isClosed;
-        }
-
-        public T pop() {
-            T last = pool.getLast();
-            pool.setPos(pool.size() - 1);
-            return last;
-        }
-
-        @Override
-        public void put(T frame) {
-            pool.add(frame);
-        }
-
-        public int size() {
-            return pool.size();
-        }
-    }
-}
-"""
-            get_signature_component(java_code)
+            store_dataset(df, args.output)
+        case "epc":  # Extract Parent Component
+            # java_code = open(args.input, "r").read()
+            # extract_signature_and_var(java_code)
+            df = pd.read_parquet(args.input, "fastparquet")
+            new_df = get_parent_signature_and_var(df, args.dir)
+            store_dataset(new_df, args.output)
