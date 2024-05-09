bb7f674ebc9f613831e5a2b15c4e1afa20334b92
3rcode <luuvanducthieu291@gmail.com>
2024-05-09 12:08:46 +0700

    Format java_data/test_generator/run.py to match linux code style :)

--- java_data/test_generator/run.py
+++ java_data/test_generator/run.py
@@ -1,8 +1,9 @@
-import pandas as pd
-from argparse import ArgumentParser
-from subprocess import run
 import logging
 import os
+from argparse import ArgumentParser
+from subprocess import run
+
+import pandas as pd
 
 parser = ArgumentParser()
 parser.add_argument("--input", dest="input")
@@ -10,31 +11,55 @@ parser.add_argument("--base-dir", dest="base_dir")
 parser.add_argument("--time-limit", dest="time_limit")
 parser.add_argument("--output-limit", dest="output_limit")
 
-logging.basicConfig(level=logging.INFO,format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
 def main(args):
     df = pd.read_parquet(args.input)
     df = df.sample(n=5, random_state=0)
-    df.to_csv("/var/data/lvdthieu/test.csv")    
-
+    df.to_csv("/var/data/lvdthieu/test.csv")
+    randoop_class_path = f"{BASE_DIR}/lib/randoop-4.3.3/randoop-all-4.3.3.jar"
+    junit_class_path = (
+        f"{BASE_DIR}/lib/hamcrest-core-1.3.jar" 
+        f":{BASE_DIR}/lib/junit-4.12.jar"
+    )
     for _, row in df.iterrows():
-        relative_path_to_classes = row["relative_path"].split("/src/main/java/")[0]
-        path_to_classes = f"""{args.base_dir}/{row["proj_name"]}/{relative_path_to_classes}""" 
-        qualified_name = row["relative_path"].split("src/main/java/")[1].replace(".java", "").replace('/', '.')
-        if row["proj_name"] == "vsch_flexmark-java":  # Temporary add
-            cmd = (f"export RANDOOP={BASE_DIR}/lib/randoop-4.3.3/randoop-all-4.3.3.jar "
-                + f"&& export JUNIT={BASE_DIR}/lib/hamcrest-core-1.3.jar:{BASE_DIR}/lib/junit-4.12.jar "
-                + f"&& cd {path_to_classes} "
-                + f"&& java -classpath .:{path_to_classes}/target/classes:{path_to_classes}/target/dependency/*:$RANDOOP randoop.main.Main gentests --testclass {qualified_name} --methodlist={path_to_classes}/methodlist.txt --output-limit=100 --time-limit {args.time_limit} --output-limit {args.output_limit}")
+        relative_path_to_pom = row["relative_path"].split("/src/main/java/")[0]
+        path_to_pom = f"{args.base_dir}/{row["proj_name"]}/{relative_path_to_pom}"
+        qualified_name = (
+            row["relative_path"]
+            .split("src/main/java/")[1]
+            .replace(".java", "")
+            .replace("/", ".")
+        )
+        # if row["proj_name"] == "vsch_flexmark-java":  # Temporary add
+        class_path = (
+            "."
+            f":{path_to_pom}/target/classes"
+            f":{path_to_pom}/target/dependency/*"
+            f":{randoop_class_path}"
+        )
+        cmd = (
+            f"cd {path_to_pom} "
+            f"&& java -classpath {class_path} randoop.main.Main gentests "
+            f"--testclass {qualified_name} "
+            f"--methodlist={path_to_pom}/methodlist.txt "
+            f"--time-limit {args.time_limit}"
+            f"--output-limit {args.output_limit}"
+        )
         # print(cmd)
-            try:
-                run(cmd, shell=True)
-            except Exception:
-                logging.info(f"""Can not gentest for {row["proj_name"]}/{row["relative_path"]}""")
-            break
-    
+        try:
+            run(cmd, shell=True)
+        except Exception:
+            logging.info(
+                f"Can not gentest for {row["proj_name"]}/{row["relative_path"]}"
+            )
+        break
+
 
 if __name__ == "__main__":
     args = parser.parse_args()
