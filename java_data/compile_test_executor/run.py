b750106f3282b61b6b0004e68d05531a956bed9a
3rcode <luuvanducthieu291@gmail.com>
2024-05-18 10:46:31 +0700

    Restructure

--- java_data/compile_test_executor/run.py
+++ java_data/compile_test_executor/run.py
@@ -1,6 +1,7 @@
 import argparse
 
 import pandas as pd
+import logging
 
 parser = argparse.ArgumentParser()
 parser.add_argument()
@@ -8,7 +9,7 @@ parser.add_argument()
 
 def main(args):
     df = pd.read_parquet(args.input)
-
+    
 
 if __name__ == "__main__":
     args = parser.parse_args()
