ad7bc75b69ec29b1111db42156e420a29acbe41f
lvdthieu <luuvanducthieu291@gmail.com>
2024-07-03 10:19:40 +0700

    remove sh files from tracking

--- java_data/processors/make_data/run.sh
+++
@@ -1,6 +0,0 @@
-
-python /home/hieuvd/lvdthieu/CodeGen/java_data/make-data/make_data.py \
-        --input /home/hieuvd/lvdthieu/java-files.parquet \
-        --dir /home/hieuvd/lvdthieu/repos/processed-projects \
-        --workers 35 \
-        --output /home/hieuvd/lvdthieu/java-dataset.parquet
